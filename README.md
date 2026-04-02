10578aa39b9dfa5eaf34d1ffe241172ac705b8f9
IsmaelTrentin <pandux66@gmail.com>
2026-04-02 10:52:15 +0200

    chore: setup repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Video Game Sales & Industry Data (1980 - 2024)
+
+Data Science Project `1`
+
+## Group Members
+
+- Horisberger Xavier
+- Trentin Ismael
+
---
+++ data/download_dataset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o ~/Downloads/video-game-sales-and-industry-data-1980-2024.zip\
+  https://www.kaggle.com/api/v1/datasets/download/bhushandivekar/video-game-sales-and-industry-data-1980-2024
